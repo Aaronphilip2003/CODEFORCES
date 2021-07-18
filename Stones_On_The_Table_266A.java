e3729499efa13a4379f209e56b298789c00d802e
Aaronphilip2003 <69317200+Aaronphilip2003@users.noreply.github.com>
2021-07-19 00:42:40 +0530

    Add files via upload

---
+++ Stones_On_The_Table_266A.java
@@ -0,0 +1,23 @@
+import java.io.*;
+public class Stones_On_The_Table_266A
+{
+	public static void main(String args[])throws IOException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int n=0;
+		n=Integer.parseInt(br.readLine());
+		String s="";
+		s=br.readLine();
+		int count=0;
+		for(int i=0,j=1;i<n&&j<n;i++,j++)
+		{
+			char ch=s.charAt(i);
+			char ch1=s.charAt(j);
+			if(ch==ch1)
+				count++;
+		}
+		System.out.println(count);
+		
+	}
+
+}
