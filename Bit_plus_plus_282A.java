fd74d49136dc811951ed64899cfa58ea088fb585
Aaronphilip2003 <69317200+Aaronphilip2003@users.noreply.github.com>
2021-07-19 10:22:01 +0530

    Add files via upload

---
+++ Bit_plus_plus_282A.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+public class Bit_plus_plus_282A
+{
+	public static void main(String args[])throws IOException
+	{
+		Scanner sc=new Scanner(System.in);
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));		
+		int operations=0;
+		int count=0;
+		String statement="";
+		operations=Integer.parseInt(br.readLine());
+		while(operations!=0)
+		{
+			statement=br.readLine();
+			if(statement.equalsIgnoreCase("X++")||statement.equalsIgnoreCase("++X"))
+			{
+				count++;
+			}
+			else if(statement.equalsIgnoreCase("X--")||statement.equalsIgnoreCase("--X"))				
+			{
+				count--;
+			}
+			operations--;
+		}
+		System.out.println(count);
+	}
+
+}
---
+++ Domino_Piling_50A.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Domino_Piling_50A 
+{
+	public static void main(String args[]) 
+	{
+		Scanner sc=new Scanner(System.in);
+		int m=0;
+		int n=0;
+		m=sc.nextInt();
+		n=sc.nextInt();
+		System.out.println((m*n)/2);
+	}
+
+}
