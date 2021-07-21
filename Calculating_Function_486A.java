334d054c237ff0a2e4044c6864298242eb78bc68
Aaronphilip2003 <69317200+Aaronphilip2003@users.noreply.github.com>
2021-07-22 02:36:20 +0530

    Add files via upload

---
+++ Calculating_Function_486A.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Calculating_Function_486A 
+{
+	public static void main(String argsp[])throws IOException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		long n=0;
+		n=Long.parseLong(br.readLine());
+		long sum=0;
+		
+		if (n%2 == 0)
+			sum = n/2;
+		else
+			sum = (-1)*(n/2 + 1);
+		
+		System.out.println(sum);
+
+	}
+
+}
