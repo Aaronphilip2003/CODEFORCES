9cc13990830d76ac6cf2ba30fa1164429d594629
Aaronphilip2003 <69317200+Aaronphilip2003@users.noreply.github.com>
2021-07-29 14:49:50 +0530

    Add files via upload

---
+++ Add_Odd_Subtract_Even_1311A.py
@@ -0,0 +1,20 @@
+t=int(input())
+while t!=0:
+  a,b = input().split()
+  a=int(a)
+  b=int(b)
+  if b>a:
+    if (b-a)%2 == 0:
+      print("2")
+    elif b==a:
+      print("0")
+    else:
+      print("1")
+  else:
+    if (b-a)%2 != 0:
+      print("2")
+    elif b==a:
+      print("0")
+    else:
+      print("1")
+  t = t-1
