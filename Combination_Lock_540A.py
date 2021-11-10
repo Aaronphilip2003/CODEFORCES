55a24e1e261c7fa82776ffc3b8b6e6cbaa8bdf5c
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2021-11-10 12:33:09 +0530

    Create Combination_Lock_540A.py

---
+++ Combination_Lock_540A.py
@@ -0,0 +1,24 @@
+def split(a):
+	return [char for char in a]
+
+n=int(input())
+wrong=input()
+correct=input()
+
+wrong_split=split(wrong)
+correct_split=split(correct)
+
+wrong_split_int=[]
+correct_split_int=[]
+sum=0
+
+for i in wrong_split:
+	wrong_split_int.append(int(i))
+
+for i in correct_split:
+	correct_split_int.append(int(i))
+
+for i in range(0,n):
+	sum+=min(10-abs(wrong_split_int[i]-correct_split_int[i]),abs(wrong_split_int[i]-correct_split_int[i]))
+
+print(sum)
