526342f01dd29038d670d7e32a4f6e5f85862ed2
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2021-11-15 19:58:29 +0530

    Create Hit_the_Lottery_996A.py

---
+++ Hit_the_Lottery_996A.py
@@ -0,0 +1,12 @@
+cash=int(input())
+copy=cash
+notes=0
+denominations=[100,20,10,5,1]
+ 
+ 
+for i in denominations :
+  if i<=cash:
+    fac=cash//i
+    cash = cash - fac*i
+    notes = notes + fac
+print(notes)
