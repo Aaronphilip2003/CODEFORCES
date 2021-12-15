168c0c3046e52fab448d47ec8657678c6f84fd9c
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2021-12-15 18:00:47 +0530

    Create Sum_Of_Round_Numbers_1352A.py

---
+++ Sum_Of_Round_Numbers_1352A.py
@@ -0,0 +1,21 @@
+test_cases=int(input())
+
+while test_cases!=0:
+
+  number=int(input())
+  count=0
+  string_number=str(number)
+  for i in string_number:
+			if i!="0":
+					count+=1
+  print(count)
+  if number<10:
+    print(number)
+  else:
+  	for i in range(len(string_number)):
+				if string_number[i]!="0":
+						print(string_number[i]+(len(string_number)-(i+1))*"0",end=" ")
+				else: 
+						pass
+  	print()
+  test_cases-=1   
