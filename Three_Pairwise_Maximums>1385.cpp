13c3d7c968496098afa1b3d83623adb9a32aeea9
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2021-12-17 12:26:47 +0530

    Create Three_Pairwise_Maximums>1385.cpp

---
+++ Three_Pairwise_Maximums>1385.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+typedef long long ll;
+using namespace std;
+int main()
+{
+    ll testcases=0;
+    cin>>testcases;
+    while (testcases!=0)
+    {
+        vector<ll>num(3);
+        for(auto &it: num) cin>>it;
+        sort(num.begin(),num.end());
+        if(num[1]!=num[2])
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+            cout<<num[0]<<" "<<num[0]<<" "<<num[2]<<endl;
+        }
+        testcases--;
+    }
+    return 0;
+}
