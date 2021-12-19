732639c59fe75958a4a9113b87821ebe715d40e5
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2021-12-20 02:17:24 +0530

    Create Long_Comparison_1613A.cpp
    Shrinking of Numbers concept

---
+++ Long_Comparison_1613A.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+ 
+int main(){
+	int t;
+	cin >> t;
+	while (t--){
+		long long x1, x2;
+		int p1, p2;
+		cin >> x1 >> p1 >> x2 >> p2;
+		int minm = min(p1, p2);
+		p1 =p1- minm;
+		p2 = p2-minm;
+		if (p1 >= 7)
+			cout << ">" << endl;
+		else if (p2 >= 7)
+			cout << "<" << endl;
+		else{
+			for (int i = 0; i < p1; ++i) 
+            x1 *= 10;
+			for (int i = 0; i < p2; ++i)
+             x2 *= 10;
+			if (x1 < x2)
+				cout << "<" << endl;
+			else if (x1 > x2)
+				cout << ">" << endl;
+			else
+				cout << "=" << endl;
+		}
+	}
+}
