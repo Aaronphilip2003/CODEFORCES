c9418f6c1baed0be4fda51c556ae36a36d0c9e4c
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2021-12-24 16:47:21 +0530

    Create XORwice_1421A.cpp

---
+++ XORwice_1421A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+typedef long long ll;
+using namespace std;
+int main()
+{
+    ll testcases,a,b;
+    cin>>testcases;
+    
+    while(testcases!=0)
+    {
+        cin>>a>>b;
+        cout<<(a^b)<<endl;
+        testcases--;
+    }
+    
+    return 0;
+}
