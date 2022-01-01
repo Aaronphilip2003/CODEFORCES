b79e9d2125d767a6f14d5668f26f1a21c6af48a1
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2022-01-01 17:36:26 +0530

    Create Prison_Break_1457A.cpp

---
+++ Prison_Break_1457A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+typedef long long ll;
+using namespace std;
+int main()
+{
+    ll t,n,m,r,c;
+    cin>>t;
+    while(t--){
+        cin>>n>>m>>r>>c;
+        ll dis = 0;
+        dis = max(dis,(r-1) + (c-1));
+        dis = max(dis,((n-r) + (c-1)));
+        dis = max(dis,((r-1) + (m-c)));
+        dis = max(dis,(n-r) + (m-c));
+        cout<<dis<<endl;
+    }
+
+    return 0;
+}
