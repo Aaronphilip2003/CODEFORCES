6d33079f7eac2ebe540fe611ff3a9f9f62b771c1
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2022-01-03 00:13:54 +0530

    Create Fake News (easy)_802G.cpp

---
+++ Fake News (easy)_802G.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+ 
+    const string t("heidi");
+    string s; cin >> s;
+    bool fake(false);
+    long ind(0);
+    for(long p = 0; p < s.size(); p++){
+        if(s[p] == t[ind]){++ind;}
+        if(ind >= t.size()){fake = true; break;}
+    }
+ 
+    cout << (fake ? "YES" : "NO") << endl;
+ 
+    return 0;
+}
